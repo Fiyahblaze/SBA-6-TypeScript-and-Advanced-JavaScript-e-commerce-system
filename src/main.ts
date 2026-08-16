473d4ee21df5a98b2ab6144721c0fdd2d2dacddf
Justin <woodsj40@gmail.com>
2026-08-16 19:08:41 -0400

    nineteenth commit

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,20 @@ import { calculateTax } from "./utils/taxCalculator";
 import { handleError } from "./utils/errorHandler";
 
 const productsContainer =
+
     document.getElementById("products");
+async function main(): Promise<void> {
+    try {
+        const data = await fetchProducts();
+
+        const products = data.slice(0, 6).map(
+            product =>
+                new Product(
+                    product.id,
+                    product.title,
+                    product.price,
+                    product.discountPercentage,
+                    product.category,
+                    product.thumbnail
+                )
+        );
